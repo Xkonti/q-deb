e9c9bbee935b04aaa1ee781d8c1c2349749849b9
Xkonti <online.xkonti@gmail.com>
2019-10-21 12:18:26 +0200

    Restore server-side (electron) code

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,45 +1,113 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, ipcMain, dialog, clipboard } from 'electron';
+import { start } from './server';
 
 /**
  * Set `__statics` path to static files in production;
  * The reason we are setting it here is that the path needs to be evaluated at runtime
  */
 if (process.env.PROD) {
-  global.__statics = require('path').join(__dirname, 'statics').replace(/\\/g, '\\\\')
+  global.__statics = require('path')
+    .join(__dirname, 'statics')
+    .replace(/\\/g, '\\\\');
 }
 
-let mainWindow
+let nextId = 0;
+function getNextId() {
+  const id = nextId;
+  nextId++;
+  return id;
+}
 
-function createWindow () {
+let mainWindow;
+
+function createWindow() {
   /**
    * Initial window options
    */
-  mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 600,
-    useContentSize: true,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+  let e = 'Creating server';
 
-  mainWindow.loadURL(process.env.APP_URL)
+  start(addMessage)
+    .then(() => {
+      mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        useContentSize: true,
+        webPreferences: {
+          nodeIntegration: true
+        }
+      });
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+      mainWindow.loadURL(process.env.APP_URL);
+      mainWindow.on('closed', () => {
+        mainWindow = null;
+      });
+    })
+    .catch(reason => {
+      dialog.showMessageBox({
+        title: 'Electron error',
+        message: reason
+      });
+    });
 }
 
-app.on('ready', createWindow)
+function addMessage(request) {
+  try {
+    newMessages.push({
+      id: getNextId(),
+      exception: request.body.exception,
+      level: request.body.level,
+      message: request.body.message,
+      source: request.body.source,
+      tags: request.body.tags,
+      timestamp: Date.parse(request.body.timestamp) || new Date()
+    });
+    return true;
+  } catch (e) {
+    newMessages.push({
+      message: 'Can not create response message: ' + e,
+      severity: 'error',
+      timestamp: new Date()
+    });
+    return false;
+  }
+}
+
+app.on('ready', () => {
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
+
+let newMessages = [];
+
+function showDialog(message) {
+  dialog.showMessageBox({
+    title: 'Got new message',
+    message: message
+  });
+}
+
+ipcMain.on('add-new-message', (event, arg) => {
+  //showDialog(`Received new message: ${JSON.stringify(arg)}`);
+  newMessages.push(arg);
+});
+
+ipcMain.on('get-new-messages', (event, arg) => {
+  //showDialog(`Sending all messages: ${JSON.stringify(newMessages)}`);
+  event.reply('new-messages', newMessages);
+  newMessages = [];
+});
+
+ipcMain.on('copyToClipboard', (event, arg) => {
+  clipboard.writeText(JSON.stringify(arg));
+});

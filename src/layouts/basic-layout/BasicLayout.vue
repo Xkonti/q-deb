a098461f67201945a5d1772a017189834f943ddf
Xkonti <online.xkonti@gmail.com>
2019-10-24 17:17:35 +0200

    refactor: create separate filter toolbar

--- src/layouts/basic-layout/BasicLayout.vue
+++ src/layouts/basic-layout/BasicLayout.vue
@@ -2,6 +2,7 @@
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white">
       <MainToolbar />
+      <FilterBar/>
     </q-header>
 
     <q-page-container>
@@ -14,9 +15,10 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import MainToolbar from '../../components/MainToolbar.vue';
+import FilterBar from '../../components/FilterBar.vue';
 
 @Component({
-  components: { MainToolbar }
+  components: { FilterBar, MainToolbar }
 })
 export default class BasicLayout extends Vue {}
 </script>
